0e8479d4c8c7da823fb47ef5f8cbd65a6966f340
connorpalermo <cjpalermo1996@gmail.com>
2024-01-27 13:44:07 -0500

    add respond to multiple pings

--- app/server.go
+++ app/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 
 	"net"
 	"os"
@@ -20,16 +22,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	buffer := make([]byte, 1024)
-	_, err = conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading from connection: ", err.Error())
-		os.Exit(1)
-	}
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
 
-	_, err = conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
-		os.Exit(1)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("End of File")
+				os.Exit(1)
+			}
+			fmt.Println(err.Error())
+		}
+		fmt.Println("Received message: ", line)
+
+		_, err = writer.WriteString("+PONG\r\n")
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+		}
+		writer.Flush()
 	}
 }
